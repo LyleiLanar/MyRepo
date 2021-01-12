909052086eb5d2c824d76f513e65dea42dc5c688
LyleiLanar <lylei.lanar@gmail.com>
2021-01-12 13:24:22 +0100

    ParosTomb elkészült

---
+++ CSharpTankönyv/WinForm/ParosTomb/ParosTomb.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace ParosTomb
+{
+
+    class ParosTomb
+    {
+        //propertik
+        private int[] Tomb { get; set; }
+        public int Length
+        {
+            get
+            {
+                return this.Tomb.Length;
+            }
+        }
+
+        //konstruktor
+        public ParosTomb(int meret)
+        {
+            Tomb = new int[meret];
+        }
+
+
+        public int this[int szam]
+        {
+            get
+            {
+                return this.Tomb[szam];
+            }
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    this.Tomb[szam] = value;
+                }
+                else
+                {
+                    throw new System.Exception("Ez a szám páratlan!!!");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<");
+            for (int i = 0; i < this.Tomb.Length - 1; i++)
+            {
+                sb.Append($"{this[i]},");
+            }
+
+            sb.Append($"{this[this.Tomb.Length-1]}> ");
+
+
+            return sb.ToString();
+        }
+
+
+    }
+}
+
---
+++ CSharpTankönyv/WinForm/ParosTomb/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ParosTomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParosTomb parosTomb = new ParosTomb(3);
+
+            for (int i = 0; i < parosTomb.Length; i++)
+            {
+                try
+                {
+                    Console.Write($"Kérlek adj be egy számot, a tömb feltöltéséhez ({i+1}/{parosTomb.Length}): ");
+                    int szam = Convert.ToInt32(Console.ReadLine());
+                    parosTomb[i] = szam; 
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nem megfelelő érték!");
+                    i--;
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    i--;
+                }
+
+            }
+
+            Console.WriteLine(parosTomb.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
